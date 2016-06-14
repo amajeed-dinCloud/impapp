9d10df0c7a2c7ac477215bc8b511c7621d5429cd
Abdul <amajeed@dincloud.com>
2016-06-15 00:09:21 +0500

    adding admin page.

--- impapp/admin_panel/views.py
+++ impapp/admin_panel/views.py
@@ -2,10 +2,14 @@ __author__ = 'Abdul'
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from impapp.update_ratings import refresh_ratings
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.template.loader import get_template
 
 @login_required
 def is_working(request):
-    return HttpResponse("*IS WORKING*")
+    return render_to_response('admin_dashboard.html', {'request': request}, context_instance=RequestContext(request))
+    # return HttpResponse("*IS WORKING*")
 
 
 @login_required
