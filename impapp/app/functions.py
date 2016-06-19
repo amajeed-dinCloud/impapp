30bbaab355fa024fffc7a75ca4c2e6fc82bb67be
Abdul <amajeed@dincloud.com>
2016-06-19 22:03:39 +0500

    call updated

--- impapp/app/functions.py
+++ impapp/app/functions.py
@@ -16,10 +16,10 @@ def update_profile_image_internal(user_obj, img_id):
 
 def make_user_response(user_obj, hide_pass=None):
     user_dict = {"name": user_obj.name, "age": user_obj.age, "city": user_obj.city, "fb_id": user_obj.fb_id,
-                 "profile_rating": user_obj.profile_rating, "is_public": 0 if user_obj.is_public else 1, "email": user_obj.email,
+                 "profile_rating": user_obj.profile_rating, "is_public": True if user_obj.is_public else False, "email": user_obj.email,
                  "user_images": make_user_images(user_obj), "ins_id": user_obj.ins_id, "password": user_obj.password,
-                 "id": user_obj.id, "is_active": 0 if user_obj.is_active else 1,
-                 "is_approved": 0 if user_obj.is_approved else 1}
+                 "id": user_obj.id, "is_active": True if user_obj.is_active else False,
+                 "is_approved": True if user_obj.is_approved else False}
 
     if hide_pass:
         del(user_dict["password"])
